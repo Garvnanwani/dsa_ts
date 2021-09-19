77948c21bc948900ac809e0a01bd62d6f00d7c6d
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-09-20 00:24:02 +0530

    tried

--- binarySearchTS.ts
+++ binarySearchTS.ts
@@ -1,24 +1,48 @@
 const array2: Number[] = [-2, -1, 0, 1, 4, 7, 8, 12, 15]
-const target: Number = 4
+const target: Number = 1
 
 
-const binarySearch = (arr: Number[], target: Number) => {
-    let start = 0
-    let end = arr.length - 1
+// const binarySearch = (arr: Number[], target: Number) => {
+//     let start = 0
+//     let end = arr.length - 1
 
-    while (start <= end) {
-        let mid = Math.floor(start + (end - start) / 2)
+//     while (start <= end) {
+//         let mid = Math.floor(start + (end - start) / 2)
 
-        if (arr[mid] === target) {
-            return mid
-        } else if (arr[mid] < target) {
-            start = mid + 1
-        } else {
-            end = mid - 1
-        }
+//         if (arr[mid] === target) {
+//             return mid
+//         } else if (arr[mid] < target) {
+//             start = mid + 1
+//         } else {
+//             end = mid - 1
+//         }
+//     }
+
+//     return -1
+// }
+
+// console.log(binarySearch(array2, target))
+
+
+const binarySearch2=(arr: Number[], target: Number) => {
+    let start=0
+    let last=arr.length-1
+
+while(start<=last) {
+    let mid= Math.floor(start + (last-start)/2)
+    if (target===arr[mid]) {
+        return mid;
+
+    }
+    else if (target<arr[mid]) {
+        last=mid-1
+
+    }else  {
+        start=mid+1
+
     }
 
-    return -1
+}return -1
 }
 
-console.log(binarySearch(array2, target))
+console.log(binarySearch2(array2, target))
--- linearSearchTS.ts
+++ linearSearchTS.ts
@@ -1,14 +1,26 @@
-const array1: Number[] = [-2, 0, 10, 4, 3, 1, 5, 7]
-const target: Number = 5
+// const array1: Number[] = [-2, 0, 10, 4, 3, 1, 5, 7]
+// const target2: Number = 5
 
-const LinearSearch = (arr: Number[], target: Number): Number => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === target) {
-            return i
-        }
-    }
+// const LinearSearch = (arr: Number[], target: Number): Number => {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === target2) {
+//             return i
+//         }
+//     }
 
-    return -1
+//     return -1
+// }
+
+// console.log(LinearSearch(array1, target))
+
+
+const array3:Number[] =[1,2,2.66,9]
+ const linearSearch=(arr:Number[], num:Number): Number=> {
+     for (let i = 0; i<arr.length; i++){
+         if (arr[i]===num){
+           return i;
+        }}
+ return -1;
 }
 
-console.log(LinearSearch(array1, target))
+console.log(linearSearch(array3,2))
